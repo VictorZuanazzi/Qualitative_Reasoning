51b90efc017108e28c51302f27261d8b78b2dc57
David Speck <david@saduras.de>
2019-03-18 12:26:58 +0100

    Mock state in main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+from quantity import Quantity
+from magnitude import Magnitude, MValue
+from derivative import Derivative, DValue
+
+def main():
+    state = {
+        "Hoose" : {
+            "Inflow" : Quantity(Magnitude(MValue.ZERO), Derivative(DValue.ZERO))
+        },
+        "Container" : {
+            "Volume" : Quantity(Magnitude(MValue.ZERO), Derivative(DValue.ZERO))
+        },
+        "Drain" : {
+            "Outflow" : Quantity(Magnitude(MValue.ZERO), Derivative(DValue.ZERO))
+        },
+    }
+
+    relations = [
+        {
+            "type" : "I+",
+            "Q1" : ("Hoose", "Inflow"),
+            "Q2" : ("Container", "Volume"),
+        },
+        {
+            "type" : "I-",
+            "Q1" : ("Drain", "Outflow"),
+            "Q2" : ("Container", "Volume"),
+        },
+        {
+            "type" : "P+",
+            "Q1" : ("Container", "Volume"),
+            "Q2" : ("Drain", "Outflow"),
+        },
+    ]
+
+if __name__ == "__main__":
+    main()
