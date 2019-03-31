105c5d708576458abf3401359a61adacad444fc9
David Speck <david@saduras.de>
2019-03-31 19:54:06 +0200

    Validation: Handle ambiguous derivatives and fix VC affecting derivaties

--- validation.py
+++ validation.py
@@ -7,10 +7,14 @@ def getRelationQuantities(state, relation):
     tail = state[entity2][quan2]
     return head, tail
 
+
 def isStateValid(state, relations):
     isValid = True
 
     # TODO fix rule interaction with same tail
+    possibleDerivatives = {}
+    magnitudes = {}
+
     for rel in relations:
         # get implemetation for relation
         func = getFunc(rel["type"], rel["args"])
@@ -21,8 +25,27 @@ def isStateValid(state, relations):
         # apply relation
         func(head, new_tail)
 
-        # check if state changed
-        if not new_tail == tail:
+        if rel["type"] == "VC":
+            if new_tail != tail:
+                magnitudes[rel["Q2"]] = new_tail.magnitude.value
+        else:
+            if not rel["Q2"] in possibleDerivatives:
+                possibleDerivatives[rel["Q2"]] = []
+
+            if not new_tail.derivative.value in possibleDerivatives[rel["Q2"]]:
+                possibleDerivatives[rel["Q2"]].append(new_tail.derivative.value)
+
+            if len(possibleDerivatives[rel["Q2"]]) == 2 and possibleDerivatives[rel["Q2"]][0]== -1 * possibleDerivatives[rel["Q2"]][1]:
+                # add derivative = 0 for continuity
+                possibleDerivatives[rel["Q2"]].append(0)
+    for rel in relations:
+        head, tail = getRelationQuantities(state, rel)
+        # check if state changed to valid option
+        if not tail.derivative.value in possibleDerivatives[rel["Q2"]]:
+            isValid = False
+            break
+
+        if rel["Q2"] in magnitudes and magnitudes[rel["Q2"]] != tail.magnitude.value:
             isValid = False
             break
 
--- validation_test.py
+++ validation_test.py
@@ -55,19 +55,15 @@ class TestValidation(unittest.TestCase):
 
     def test_isStateValid_singleRelation_invalid(self):
         state = {
-            "Container" : {
-                "Volume" : Quantity(Magnitude(MValue.ZERO), Derivative(DValue.PLUS))
-            },
-            "Drain" : {
-                "Outflow" : Quantity(Magnitude(MValue.ZERO), Derivative(DValue.ZERO))
-            },
+            "A" : { "Q" : Quantity(Magnitude(MValue.ZERO), Derivative(DValue.PLUS)) },
+            "B" : { "Q" : Quantity(Magnitude(MValue.ZERO), Derivative(DValue.ZERO)) },
         }
         relations = [
             {
                 "type" : "P+",
                 "args" : None,
-                "Q1" : ("Container", "Volume"),
-                "Q2" : ("Drain", "Outflow"),
+                "Q1" : ("A", "Q"),
+                "Q2" : ("B", "Q"),
             },
         ]
 
@@ -99,6 +95,41 @@ class TestValidation(unittest.TestCase):
 
         self.assertEqual(isStateValid(state, relations), True)
 
+    def test_isStateValid_multiRelation_sameTail_ambiguous(self):
+        state1 = {
+            "A" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.MINUS)) },
+            "B" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.MINUS)) },
+            "C" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.MINUS)) },
+        }
+        state2 = {
+            "A" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.MINUS)) },
+            "B" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.ZERO)) },
+            "C" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.MINUS)) },
+        }
+        state3 = {
+            "A" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.MINUS)) },
+            "B" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.PLUS)) },
+            "C" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.MINUS)) },
+        }
+        relations = [
+            {
+                "type" : "P+",
+                "args" : None,
+                "Q1" : ("A", "Q"),
+                "Q2" : ("B", "Q"),
+            },
+            {
+                "type" : "I+",
+                "args" : None,
+                "Q1" : ("C", "Q"),
+                "Q2" : ("B", "Q"),
+            },
+        ]
+
+        self.assertEqual(isStateValid(state1, relations), True)
+        self.assertEqual(isStateValid(state2, relations), True)
+        self.assertEqual(isStateValid(state3, relations), True)
+
     def test_isStateValid_multiRelation_invalid(self):
         state = {
             "Container" : {
@@ -120,6 +151,29 @@ class TestValidation(unittest.TestCase):
                 "args" : None,
                 "Q1" : ("Drain", "Outflow"),
                 "Q2" : ("Container", "Volume"),
+            },
+        ]
+
+        self.assertEqual(isStateValid(state, relations), False)
+
+    def test_isStateValid_VC_not_affect_derivative(self):
+        state = {
+            "A" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.PLUS)) },
+            "B" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.MINUS)) },
+            "C" : { "Q" : Quantity(Magnitude(MValue.PLUS), Derivative(DValue.MINUS)) },
+        }
+        relations = [
+            {
+                "type" : "P+",
+                "args" : None,
+                "Q1" : ("A", "Q"),
+                "Q2" : ("B", "Q"),
+            },
+            {
+                "type" : "VC",
+                "args" : MValue.MAX,
+                "Q1" : ("C", "Q"),
+                "Q2" : ("B", "Q"),
             },
         ]
 
