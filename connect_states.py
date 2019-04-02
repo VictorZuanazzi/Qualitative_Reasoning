0d1d4b43ee8621aabee65c026f86bfcf0f20430b
Victor <victorzuanazzi@gmail.com>
2019-04-02 23:01:56 +0200

    naivelly working connection of external influcences

--- connect_states.py
+++ connect_states.py
@@ -42,8 +42,6 @@ def state_copy(state):
 
     return c_s
         
-        
-        
 def add_directional_connection(prev_s, next_s):
     """makes the directional connections between states.
     Inputs:
@@ -67,7 +65,36 @@ def add_directional_connection(prev_s, next_s):
         next_s["prev"].add(prev_s["id"])
     else:
         next_s["prev"] = {prev_s["id"]}
+
+def compare_states_except(state_1, state_2, exc_quantity = "Inflow"):
+    """compare if the quantities of the states are the same except for the
+    given quantity. Non-quantities are ignored.
+    Input:
+        state_1, state_2: (dict state format) states to be compared.
+        exc_quantity: quantity to be ignored during the comparison.
         
+    Ouput (bool) True if the states are the same (except for the excluded 
+          quantity) or False if at least one quantity differs. 
+    """
+    
+    for entity in state_1:
+        
+        #non quantities are ignored
+        if (entity == "id") | (entity == "prev") | (entity == "next"):
+            continue
+        
+        for q in state_1[entity]:
+            
+            #ignores the defined quantity
+            if q == exc_quantity:
+                continue
+            
+            if state_1[entity][q] != state_2[entity][q]:
+                #if one of the quantities is different, then the state is different.
+                return False
+                
+    return True
+    
 
 def list_to_state(list_state, a_state):
     """transforms a list with values correctly ordered for the state into the
@@ -97,7 +124,6 @@ def list_to_state(list_state, a_state):
             
     return c_state
     
-
 def connect_states(unconnected_states):
     """create phisically possible connection between states.
     NOT FULLU WORKING YET"""
@@ -109,53 +135,75 @@ def connect_states(unconnected_states):
     #For each state, check if it is possible to transition to the other state.
     for s_1 in unconnected_states:
         
+        #obligatory transitions (effects of the derivatives):
+        #kinda working but there are some weird stuff, 
+        #commented to debug external influences
+        
         #gets all the possibilities of transitions considering all quantities 
         #of entities.
-        quantities = []
-        for entity_1 in s_1.copy():
-            
-            if (entity_1 == "id") | (entity_1 == "prev") | (entity_1 == "next"):
-                continue
-            
-            for q in s_1[entity_1]:       
-                #!!! applyDerivative changes the upper bound of inflow !!!
-                quantities.append(Quantity.applyDerivative(s_1[entity_1][q]))
-          
-        
-        all_poss = list(product(*quantities))
-        
-        #compares all_possibilities of next states with the posssible next states.
-        for maybe_next_state in all_poss:
-            #converts the list into a state description
-            
-            for s_2 in unconnected_states:
-                
-                #it is slow, but it works!
-                new_s = list_to_state(maybe_next_state, s_2)
-
-                if new_s == s_2:
-                    add_directional_connection(s_1, s_2)
-                    
-                    
-                    
+#        quantities = []
+#        for entity_1 in s_1.copy():
+#            
+#            if (entity_1 == "id") | (entity_1 == "prev") | (entity_1 == "next"):
+#                continue
+#            
+#            for q in s_1[entity_1]:       
+#                #!!! applyDerivative changes the upper bound of inflow !!!
+#                quantities.append(Quantity.applyDerivative(s_1[entity_1][q]))
+#          
+#        #get all combinations of possible next states.
+#        all_poss = list(product(*quantities))
+#        
+#        #compares all_possibilities of next states with the posssible next states.
+#        for maybe_next_state in all_poss:
+#            
+#            for s_2 in unconnected_states:
 #                
-#                #check all quantities of the state before making the connection
-#                for entity_2 in s_2:
+#                #it is slow, but it works! (and I can spare that millisecond)
+#                new_s = list_to_state(maybe_next_state, s_2)
+#
+#                if new_s == s_2:
 #                    
-#                    if (entity_2 == "id") | (entity_2 == "prev") | (entity_2 == "next"):
-#                        continue
-#                    
-#                    for q_2 in s_2[entity_2]:
-#                        print("generated state:\n" new_s[entity_2][q_2])
-#                        print("compared state:\n" s_2[entity_2][q_2])
-#                        if s_2[entity_2][q_2] == new_s[entity_2][q_2]:
-#                            continue
-#                        else:
-#                            connect = False
-#                            
-#                if connect:
-#                    add_directional_connection(s_1, s_2)
+#                    #connects states
+#                    add_directional_connection(s_1, s_2) 
+        
+        #external influences:
+        
+        #hardcoded because I want to be over with it.
+        #it works, but there are some links to impossible states.
+        #best way towards robust code is to specify the rules between the quantities.
+        for s_2 in unconnected_states:
+            
+            #open tap
+            #if the tap is closed
+            if s_1["Hoose"]["Inflow"].magnitude.value == 0:
+                if s_1["Hoose"]["Inflow"].derivative.value == 0:
+                    
+                    #if the tap is opening
+                    if s_2["Hoose"]["Inflow"].magnitude.value == 0:
+                        if s_2["Hoose"]["Inflow"].derivative.value == 1:
                             
+                            #if the rest of the state is the same
+                            if compare_states_except(s_1, s_2, exc_quantity = "Inflow"):
+                                add_directional_connection(s_1, s_2) 
+            
+            #close tap
+            #if the tap is open
+            if s_1["Hoose"]["Inflow"].magnitude.value == 1:
+                if s_1["Hoose"]["Inflow"].derivative.value == 0:
+                    
+                    #if the tap is closing
+                    if s_2["Hoose"]["Inflow"].magnitude.value == 1:
+                        if s_2["Hoose"]["Inflow"].derivative.value == -1:
+                            
+                            #if the rest of the state is the same
+                            if compare_states_except(s_1, s_2, exc_quantity = "Inflow"):
+                                add_directional_connection(s_1, s_2) 
+            
+            
+        
+        
+        
         
         
     return unconnected_states
