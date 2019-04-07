776caa31b99add6c5a47e25af29f4eb309f5cb9f
Victor <victorzuanazzi@gmail.com>
2019-04-07 15:49:23 +0200

    included the extras

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from quantity import Quantity
 from magnitude import Magnitude, MValue
 from derivative import Derivative, DValue
 from overgeneration import over_generate
-from validation import isStateValid
+from validation import isStateValid, pruneImpossibleStates
 from connect_states import connect_states
 from graph_maker import make_state_graph
 
@@ -24,6 +24,18 @@ def main():
             "type" : "P+",
             "args" : None,
             "Q1" : ("Container", "Volume"),
+            "Q2" : ("Container", "Height"),
+        },
+        {
+            "type" : "P+",
+            "args" : None,
+            "Q1" : ("Container", "Height"),
+            "Q2" : ("Container", "Pressure"),
+        },
+        {
+            "type" : "P+",
+            "args" : None,
+            "Q1" : ("Container", "Pressure"),
             "Q2" : ("Drain", "Outflow"),
         },
         {
@@ -55,11 +67,28 @@ def main():
             "args" : 1,
             "Q1" : ("Hoose", "Inflow"),
             "Q2" : ("Hoose", "Inflow"),
-        }
+        },
     ]
-
-    states = over_generate()
-    pruned_states = [s for s in states if isStateValid(s, relations)]
+    
+    mags = list(map(int, MValue))
+    ders = list(map(int, DValue))
+    
+    blue_print = {
+            "Hoose" : {
+                "Inflow" : ([0,1], ders)
+            },
+            "Container" : {
+                "Volume" : (mags, ders),
+                "Height" : (mags, ders),
+                "Pressure" : (mags, ders),
+            },
+            "Drain" : {
+                "Outflow" : (mags, ders)
+            },
+        }
+    
+    states = over_generate(blue_print=blue_print)
+    pruned_states = pruneImpossibleStates(states, relations) #[s for s in states if isStateValid(s, relations)]
     connected_states = connect_states(pruned_states)
     
     #print("\n".join([str(s) for s in states]))
